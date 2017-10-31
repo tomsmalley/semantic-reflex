a4e29d05c0cb0cd002980b118e2ffc5cfe3f0fbf
Tom Smalley <tfsmalley@gmail.com>
2017-10-31 15:38:45 +0000

    Change build system to reflex project

--- default.nix
+++ default.nix
@@ -1,43 +1,13 @@
-{ reflex-platform ? ../reflex-platform/. , ghcjs ? false, runCC ? true }:
-let
+{ runCC ? true }:
 
-  dontCheckPackages = [
-    "lens"
-    "jsaddle-warp"
-  ];
-
-  pkgs = import <nixpkgs> { };
-
-  makeOverrides = function: names: self: super:
-    let toPackage = name: {
-          inherit name;
-          value = function super.${name};
-        };
-    in builtins.listToAttrs (map toPackage names);
-
-  manualOverrides = self: super: rec {
+(import ./reflex-platform {}).project ({ pkgs, ... }: {
+  packages = {
+    semantic-reflex = ./semantic-reflex;
+    example = ./example;
   };
 
-  composeExtensionsList = pkgs.lib.fold pkgs.lib.composeExtensions (_: _: { });
-  doOverrides = haskellPackages: haskellPackages.override {
-    overrides = composeExtensionsList [
-      generatedOverrides
-      (makeOverrides pkgs.haskell.lib.dontCheck dontCheckPackages)
-      manualOverrides
-    ];
+  shells = {
+    ghc = ["semantic-reflex" "example"];
+    ghcjs = ["semantic-reflex" "example"];
   };
-  generatedOverrides = self: super:
-    let toPackage = file: _: {
-          name = builtins.replaceStrings [ ".nix" ] [ "" ] file;
-          value = self.callPackage (./. + "/nix/${file}") { };
-        };
-    in pkgs.lib.mapAttrs' toPackage (builtins.readDir ./nix);
-
-  platform = import reflex-platform { };
-  compiler = doOverrides (if ghcjs then platform.ghcjs else platform.ghc);
-
-in rec {
-  semantic-reflex = compiler.callPackage ./semantic-reflex { inherit ghcjs; };
-  example = compiler.callPackage ./example { inherit ghcjs runCC semantic-reflex; };
-}
-
+})
