47b223ef9bc74d83c4178412ac46e7fa852eca2d
John Ericson <John.Ericson@Obsidian.Systems>
2018-10-25 12:56:23 -0400

    Remove dumped splices

--- semantic-reflex/src/Reflex/Dom/SemanticUI/Flag.th.hs
+++
@@ -1,6 +0,0 @@
--- src/Reflex/Dom/SemanticUI/Flag.hs:28:1-62: Splicing declarations
-flagConfig_elConfig ::
-  forall t_a1ydZ. Iso' (FlagConfig t_a1ydZ) (ActiveElConfig t_a1ydZ)
-flagConfig_elConfig
-  = iso (\ (FlagConfig x_a1yxa) -> x_a1yxa) FlagConfig
-{-# INLINE flagConfig_elConfig #-}
